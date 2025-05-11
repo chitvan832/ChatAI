60aa5440c329a21f79e3470c719ab94d8fa17eaa
chitvan <chitvan832@gmail.com>
2025-05-11 20:00:54 +0530

    Chat using Cohere achieved, resolve empty message content error

--- ChatAI/ChatAIApp.swift
+++ ChatAI/ChatAIApp.swift
@@ -10,6 +10,15 @@ import SwiftData
 
 @main
 struct ChatAIApp: App {
+    private let apiKey: String
+    
+    init() {
+        self.apiKey = Config.cohereAPIKey
+//        self.apiKey = "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"
+//        //Poo Account
+//        self.apiKey = "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"
+    }
+    
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Message.self,
@@ -25,7 +34,7 @@ struct ChatAIApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ChatView()
+            ChatView(apiKey: apiKey)
         }
         .modelContainer(sharedModelContainer)
     }
